be8fb9beb89878410d4bd6686f3d0e76982f0e19
Vargas, Jorge L <jorge.vargas@thermofisher.com>
2017-07-18 12:34:22 -0700

    rename app.js to app.jsx

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var SeqStudio = artifacts.require("./SeqStudio.sol");
+var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SeqStudio);
+  deployer.deploy(SimpleStorage);
 };
