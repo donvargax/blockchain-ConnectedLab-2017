b846ef3c7fb779d5ca25233cc1cc25f1b7c09f8f
Vargas, Jorge L <jorge.vargas@thermofisher.com>
2017-07-18 12:09:51 -0700

    improve it

--- contracts/SeqStudio.sol
+++ contracts/SeqStudio.sol
@@ -7,6 +7,8 @@ contract SeqStudio {
     string status;
     string fileIds;
 
+    event SampleFilesAvailable(string deviceId, string injectionId, string status, string fileIds);
+
     function setSampleFileId(string _deviceId, string _status, string _injectionId, string _fileIds) {
         deviceId = _deviceId;
         status = _status;
@@ -15,8 +17,9 @@ contract SeqStudio {
 
         SampleFilesAvailable(deviceId, injectionId, status, fileIds);
     }
+
     function getFileIds() returns (string) {
         return fileIds;
     }
-    event SampleFilesAvailable(string deviceId, string injectionId, string status, string fileIds);
+
 }
